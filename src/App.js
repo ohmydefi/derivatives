e12ebfbb18aea7983dac4ced9d8a6042766c0e18
Robsonsjre <robsonsjre@gmail.com>
2020-02-15 02:15:10 -0700

    added netlify config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { GlobalStyles } from 'theme'
+import { Header } from 'components'
+import { useWeb3Provider, Web3Context } from 'hooks/web3'
+import { Dashboard } from 'pages'
 
-function App() {
+const basename = process.env.NODE_ENV === 'development' ? '/' : '/option-deployer'
+
+function App () {
+  const { web3 } = useWeb3Provider()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Web3Context.Provider value={web3}>
+      <GlobalStyles />
+      <Router basename={basename}>
+        <Header />
+        <Dashboard />
+      </Router>
+    </Web3Context.Provider>
+  )
 }
 
-export default App;
+export default App
