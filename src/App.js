ab17d0346e1443e791ccf81d09669a575fa27909
Robsonsjre <robsonsjre@gmail.com>
2020-02-16 03:48:51 -0700

    added fortmatic

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ import { useWeb3Provider, Web3Context } from 'hooks/web3'
 import { Dashboard, Landing } from 'pages'
 
 function App () {
-  const { web3 } = useWeb3Provider()
+  const { web3, enableFortmatic, enableMetamask } = useWeb3Provider(true)
+
+  const enableProvider = type => {
+    if (type === 'fortmatic') {
+      enableFortmatic()
+    } else {
+      enableMetamask()
+    }
+  }
 
   return (
     <Web3Context.Provider value={web3}>
       <GlobalStyles />
       <Router>
-        <Header />
+        <Header onEnable={enableProvider} />
         <Landing />
         <Dashboard />
         <Footer />
